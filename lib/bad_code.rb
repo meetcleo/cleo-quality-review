f77757108173d969cdebe7364f003f49c404acd0
Gavin Morrice <gavin@gavinmorrice.com>
2026-05-19 18:27:05 +0100

    Add performance issue to bad_code file
    Fasterer should report an issue with Hash.keys.each

--- lib/bad_code.rb
+++ lib/bad_code.rb
@@ -2,7 +2,10 @@ class BadCode
   def a
     [{}].map.each do |f|
       puts f
-      f.first
+      # Added a line to see if Fasterer would catch it?
+      f.keys.each do |k|
+        puts k
+      end
     end
   end
 end
