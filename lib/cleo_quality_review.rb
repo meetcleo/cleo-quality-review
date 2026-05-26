d831b3684f619037fb5f65d431e0efdb8c2cf4a0
Gavin Morrice <gavin@gavinmorrice.com>
2026-05-26 17:37:54 +0100

    Extract LLM provider registry into root module
    Don't bake this as a default into the class itself

--- lib/cleo_quality_review.rb
+++ lib/cleo_quality_review.rb
@@ -6,8 +6,15 @@ module CleoQualityReview
   require_relative "cleo_quality_review/version"
   require_relative "cleo_quality_review/check_registry"
   require_relative "cleo_quality_review/checks"
+  require_relative "cleo_quality_review/llm_provider_registry"
 
+  ##
+  # Register all supported tools for analysing code here
   CheckRegistry.register("Reek", "Checks::Reek", tool_type: :smell_detection)
   CheckRegistry.register("Flog", "Checks::Flog", tool_type: :complexity)
   CheckRegistry.register("Fasterer", "Checks::Fasterer", tool_type: :performance)
+
+  ##
+  # Register all supported LLM APIs for formatting output here
+  LlmProviderRegistry.register( "openai", :OpenAiLlmProvider)
 end
